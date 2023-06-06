1aaa66bc1950dc523d9da460140ced0d2da78953
Wagner Bonfim <6979340+wagfim@users.noreply.github.com>
2023-06-05 21:09:15 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+function multiply(num1, num2){
+    return num1 * num2;
+}
+let contador = 0;
+document.querySelector("img").addEventListener("click", function() {
+    console.log("teste: ",contador++);
+});
+
